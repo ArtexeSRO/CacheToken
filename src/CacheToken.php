d612e3e8190fc930f4c392400e113a72d1272030
Vladimir Vrab <iNviNho>
2017-10-18 15:24:22 +0200

    first init

---
+++ src/CacheToken.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Artexe;
+
+use Nette\Caching\Cache,
+    Nette\Utils\DateTime,
+    Nette\Caching\IStorage;
+
+/**
+ * Description of AutoSynchro
+ *
+ * @author Vladimír Vráb <www.artexe.sk>
+ */
+class CacheToken {
+    
+    /** @var Cache */
+    private $cache;
+    
+    /** @var string */
+    private $name;
+    
+    public function __construct(IStorage $storage, $name) {
+        $this->cache = new Cache($storage, $name);        
+    }
+    
+    public function getTimeStamp($isDebug = FALSE) {
+        
+       $timestamp = $this->cache->load($this->name);
+        if (is_null($timestamp) || $isDebug) {
+            $datetime = new DateTime;
+            $timestamp = $this->cache->save($this->name, $datetime->getTimestamp());
+        }    
+       
+        return $timestamp;
+    }
+    
+}
